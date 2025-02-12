401c4375da12168d3b9c73d6191239f881c1f52e
Scipio Wright <scipiowright@gmail.com>
2025-02-11 21:14:13 -0500

    Refactor a bunch of stuff to reduce yellow squiggly lines and yellow squiggly lines in my heart

--- worlds/lunacid/OptionGroups.py
+++ worlds/lunacid/OptionGroups.py
@@ -1,7 +1,8 @@
 from Options import DeathLink, ProgressionBalancing, Accessibility, OptionGroup
-from .Options import (Ending, Class, EntranceRandomization, Experience, WeaponExperience, RandomElements, RequiredStrangeCoins, TotalStrangeCoins,
-                      Shopsanity, Dropsanity, Quenchsanity, EtnasPupil, NormalizedDrops, SecretDoorLock, SwitchLocks, DoorLocks,
-                      TrapPercent, ItemColors, CustomClass, Filler, Traps)
+from .Options import (Ending, Class, EntranceRandomization, Experience, WeaponExperience, RandomElements,
+                      RequiredStrangeCoins, TotalStrangeCoins, Shopsanity, Dropsanity, Quenchsanity, EtnasPupil,
+                      NormalizedDrops, SecretDoorLock, SwitchLocks, DoorLocks, TrapPercent, ItemColors, CustomClass,
+                      Filler, Traps)
 
 lunacid_option_groups = [
     OptionGroup("General", [
